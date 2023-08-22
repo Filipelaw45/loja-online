f8f110e124b5265a7776a52f67386f6690a0f34d
Filipelaw45 <filipe.law@gmail.com>
2023-08-22 10:14:41 -0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/header'
 import Body from './components/body'
-import Footer from './components/footer'
 import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
     <Body/>
-    <Footer/>
   </React.StrictMode>,
 )
