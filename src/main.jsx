90bfbc64ad065c202f09c2d5fbcb37a07fde2342
Filipelaw45 <filipe.law@gmail.com>
2023-08-22 09:53:14 -0300

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/header'
-import Body from './components/body'
-import Footer from './components/footer'
+import Header from './components/header.jsx'
+import Body from './components/header.jsx'
+import Footer from './components/header.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
